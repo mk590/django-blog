89c5600a30704f5cd5e7c40beb28e94239530336
Mohit <lalpanda6@gmail.com>
2023-07-02 20:28:10 +0530

    again trying to depoly

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'blog_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
@@ -170,7 +170,7 @@ LOGIN_URL='login'
 
 
 # vrcel deployment
-ALLOWED_HOSTS = ['.vercel.app'] # Allow *.vercel.app
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1'] # Allow *.vercel.app
 
 # commenting out the databases for this 
-DATABASES = {}
+# DATABASES = {}
